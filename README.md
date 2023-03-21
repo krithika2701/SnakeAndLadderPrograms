38391dde9a86d99b4122cd063a2a1d1af88f2a5c
krithika2701 <shirisharudrarapu21@gmail.com>
2023-03-21 12:59:14 +0530

    Add:README.md and welcome message for snake and ladder program

---
+++ README.md
@@ -0,0 +1,2 @@
+# Snake And Ladder Programs
+# Snake And Ladder Programs
---
+++ SnakeAndLadderPrograms.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladderprograms;
+
+/* Welcome massage for snake and ladder programs*/
+
+public class SnakeAndLadderPrograms {
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake and ladder programs");
+
+	}
+
+}
